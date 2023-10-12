fb2c0147ab1a7f133f7557e987c8d7da1c131e05
MehdiBenIT <med.ben.98@gmail.com>
2023-10-11 20:53:33 -0400

    new main file

--- TERRAFORM-AWS-INFRA-PROJECTS/main.tf
+++ TERRAFORM-AWS-INFRA-PROJECTS/main.tf
@@ -6,6 +6,7 @@ provider "aws" {
 resource "aws_instance" "test_tf" {
   ami = "ami-053b0d53c279acc90"
   instance_type = "t2.micro"
+  key_name = "asfar_key.pem"
 
   tags = {
     Name = "NextCloud-QuickDeploy-${formatdate("YYYY-MM-DD", timestamp())}"
